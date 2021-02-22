9307e7f817ea01c7ac8670582c169abdb94249a3
PuSH-qaa <fedoseev_28@mail.ru>
2021-02-22 20:37:25 +0500

    Add project files.

---
+++ ProjectTask4/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProjectTask4
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
